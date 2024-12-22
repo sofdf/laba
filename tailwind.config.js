08de2aa3dbff9146304aaccd9e61f49dda4bdd13
sofdf <sofiaaleshina@mail.ru>
2024-12-22 23:21:47 +0300

    м

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path based on your project structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
